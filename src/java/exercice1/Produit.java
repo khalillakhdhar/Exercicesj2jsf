b8418c1e9796590086fce4085a1a52bd54b88de4
khalil <khalillakhdharatc@gmail.com>
2022-03-01 10:21:13 +0100

    code

---
+++ src/java/exercice1/Produit.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercice1;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author khali
+ */
+@ManagedBean
+@RequestScoped
+public class Produit {
+    private String titre,expiration,code;
+    private double prix,prixfinale;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public double getPrixfinale() {
+        return prixfinale;
+    }
+
+    public void setPrixfinale(double prixfinale) {
+        this.prixfinale = prixfinale;
+    }
+    public void calculefinale()
+    {
+    this.prixfinale=this.prix*1.12;
+    
+    
+    }
+    
+    
+    
+    
+}
